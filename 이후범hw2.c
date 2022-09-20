25824b6dc27e6551b6511176abc29fbe773a5931
Yi-hu-beom <heeg3000@naver.com>
2022-09-20 19:03:33 +0900

    Add Text File [이후범hw2.c]

---
+++ 이후범hw2.c
@@ -0,0 +1,7 @@
+﻿빌드 시작...
+1>------ 빌드 시작: 프로젝트: Project8, 구성: Debug x64 ------
+1>소스212.c
+1>C:\Users\HIMART\OneDrive\바탕 화면\비주얼스튜디오\Project8\Project8\소스212.c(8,2): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>Project8.vcxproj -> C:\Users\HIMART\OneDrive\바탕 화면\비주얼스튜디오\Project8\x64\Debug\Project8.exe
+1>"Project8.vcxproj" 프로젝트를 빌드했습니다.
+========== 빌드: 1 성공, 0 실패, 0 최신 업데이트, 0 건너뛰기 ==========
