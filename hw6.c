48506ea359aa9ad450de4515a384534ea91d7889
Yi-hu-beom <heeg3000@naver.com>
2022-11-03 01:16:50 +0900

    Add Text File[hw6.c]

---
+++ hw6.c
@@ -0,0 +1,8 @@
+﻿빌드 시작...
+1>------ 빌드 시작: 프로젝트: Project3, 구성: Debug x64 ------
+1>소스.cpp
+1>C:\Users\HIMART\source\repos\Project3\Project3\소스.cpp(28,9): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>C:\Users\HIMART\source\repos\Project3\Project3\소스.cpp(23,9): warning C4101: 'i' :참조되지 않은 지역 변수입니다.
+1>Project3.vcxproj -> C:\Users\HIMART\source\repos\Project3\x64\Debug\Project3.exe
+1>"Project3.vcxproj" 프로젝트를 빌드했습니다.
+========== 빌드: 1 성공, 0 실패, 0 최신 업데이트, 0 건너뛰기 ==========
